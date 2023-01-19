52fca4e37c499cb65c9e6057ce0443ce3a705d75
younes laptop <you@example.com>
2023-01-19 18:00:29 +0330

    fix navigation bottom from artist click to search fragment and back from it

--- app/src/main/java/com/example/musicplayer/HomeActivity.java
+++ app/src/main/java/com/example/musicplayer/HomeActivity.java
@@ -5,6 +5,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.musicplayer.api.RetrofitClient;
 import com.example.musicplayer.api.RetrofitInterface;
@@ -23,6 +24,10 @@ public class HomeActivity extends AppCompatActivity {
     private BottomNavigationView bottomNav;
 
     private Deque<Integer> integerDeque = new ArrayDeque<>(2);
+    private boolean flag = true;
+
+    private boolean isTrendArtist;
+    private static String artistName;
 
 
     @Override
@@ -32,29 +37,77 @@ public class HomeActivity extends AppCompatActivity {
 
         init();
 
+
         onTrendArtistClicked = new OnTrendArtistClicked() {
             @Override
             public void onClick(String artistName) {
-                bottomNav.getMenu().getItem(1).setChecked(true);
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, SearchFragment.newInstance(artistName)).addToBackStack(null).commit();
+                isTrendArtist = true;
+                HomeActivity.artistName = artistName;
+                bottomNav.setSelectedItemId(R.id.menu_search);
             }
         };
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, HomeFragment.newInstance(onTrendArtistClicked)).commit();
+
+        integerDeque.push(R.id.menu_home);
+        loadFragment(HomeFragment.newInstance(onTrendArtistClicked));
+        bottomNav.setSelectedItemId(R.id.menu_home);
+
+
         bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.menu_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, HomeFragment.newInstance(onTrendArtistClicked)).addToBackStack(null).commit();
-                        break;
-                    case R.id.menu_search:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, SearchFragment.newInstance()).addToBackStack(null).commit();
-                        break;
+                int id = item.getItemId();
+                if (integerDeque.contains(id)) {
+                    if (id == R.id.menu_home) {
+                        if (integerDeque.size() != 1) {
+                            if (flag) {
+                                integerDeque.addFirst(R.id.menu_home);
+                                flag = false;
+                            }
+                        }
+                    }
+                    integerDeque.remove(id);
                 }
+                integerDeque.push(id);
+                loadFragment(getFragment(item.getItemId()));
                 return true;
             }
         });
+    }
+
+    private Fragment getFragment(int itemId) {
+        switch (itemId) {
+            case R.id.menu_home:
+                bottomNav.getMenu().getItem(0).setChecked(true);
+                artistName = "";
+                isTrendArtist = false;
+                return HomeFragment.newInstance(onTrendArtistClicked);
+            case R.id.menu_search:
+                bottomNav.getMenu().getItem(1).setChecked(true);
+                if (isTrendArtist) {
+                    return SearchFragment.newInstance(artistName);
+                }
+                return SearchFragment.newInstance();
+        }
+        bottomNav.getMenu().getItem(0).setChecked(true);
+        return HomeFragment.newInstance(onTrendArtistClicked);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_container, fragment, fragment.getClass().getSimpleName())
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        integerDeque.pop();
+        if (!integerDeque.isEmpty()) {
+            loadFragment(getFragment(integerDeque.peek()));
+        } else {
+            finish();
+        }
     }
 
     private void init() {
